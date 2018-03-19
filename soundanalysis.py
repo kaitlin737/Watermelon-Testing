b72d0804eca351cf49ec62f478fc38ac3f89eb5c
Sarah Harvey <desertriverjem@gmail.com>
2018-03-19 14:39:20 -0600

    Add files via upload

--- soundanalysis.py
+++ soundanalysis.py
@@ -7,6 +7,7 @@ from scipy.fftpack import fft
 
 import sys
 
+import _thread
 from School.softwareEngineering.AgbotRipeness.soundout import *
 
 
@@ -30,13 +31,34 @@ class AudioStream(object):
 
     def start(self):
         data = self.stream.read(self.CHUNK)
-        print(data)
+        data_int = struct.unpack(str(2*self.CHUNK) + 'B', data)
+        self.data_int = data_int
+        print(len(data_int))
+        print(data_int)
+
+    def analyze(self):
+        highbound = 255
+        lowbound = 10
+        ripenum = 0
+        for i in self.data_int:
+            if i <= highbound:
+                if i >= lowbound:
+                    ripenum += 1
+        print(ripenum / len(self.data_int))
 
 
 if __name__ == '__main__':
     audio_app = AudioStream()
-    audio_app.start()
     audio_out = PlayAudio()
-    audio_out.start()
+    # audio_app.start()
+    # audio_out.start()
+    try:
+        _thread.start_new_thread(audio_app.start(), ('thread-1', 2, ))
+        _thread.start_new_thread(audio_out.start(), ('thread-2', 4, ))
+    except:
+        print("unable to start thread.")
+
+    audio_app.analyze()
+
 
 
