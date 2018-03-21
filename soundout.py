c560a4a3dd3be9c1d0301dd2dff5824bd28a8897
lanstine <laanstine@mavs.coloradomesa.edu>
2018-03-21 14:47:25 -0600

    small change, format

--- soundout.py
+++ soundout.py
@@ -12,7 +12,7 @@ fs = 44100       # sampling rate, Hz, must be integer
 duration = 10.0   # in seconds, may be float
 f = 440.0        # sine frequency, Hz, may be float
 
-# generate samples, note conversion to float32 array
+# generate sample stream
 samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32).tobytes()
 
 # for paFloat32 sample values must be in range [-1.0, 1.0]
